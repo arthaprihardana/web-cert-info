d9f944804fab461d4d519eb9a5484b6873481c79
Artha Prihardana <artha.prihardana@ai.astra.co.id>
2019-08-26 17:10:47 +0700

    init

---
+++ check.js
@@ -0,0 +1,95 @@
+/*
+ * @Author: Artha Prihardana 
+ * @Date: 2019-08-26 17:07:37 
+ * @Last Modified by:   Artha Prihardana 
+ * @Last Modified time: 2019-08-26 17:07:37 
+ */
+var https = require('https');
+
+function isEmpty(object) {
+  for (var prop in object) {
+    if (object.hasOwnProperty(prop)) return false;
+  }
+
+  return true;
+}
+
+function pemEncode(str, n) {
+  var ret = [];
+
+  for (var i = 1; i <= str.length; i++) {
+    ret.push(str[i - 1]);
+    var mod = i % n;
+
+    if (mod === 0) {
+      ret.push('\n');
+    }
+  }
+
+  var returnString = `-----BEGIN CERTIFICATE-----\n${ret.join('')}\n-----END CERTIFICATE-----`;
+
+  return returnString;
+}
+
+function getOptions(url, port, protocol) {
+  return {
+    hostname: url,
+    agent: false,
+    rejectUnauthorized: false,
+    ciphers: 'ALL',
+    port,
+    protocol
+  };
+}
+
+function validateUrl(url) {
+  if (url.length <= 0 || typeof url !== 'string') {
+    throw Error('url ga valid');
+  }
+}
+
+function handleRequest(options, resolve, reject) {
+  return https.get(options, function(res) {
+    var certificate = res.socket.getPeerCertificate();
+
+    if (isEmpty(certificate) || certificate === null) {
+      reject({ message: 'ga ada sertifikat' });
+    } else {
+      if (certificate.raw) {
+        certificate.pemEncoded = pemEncode(certificate.raw.toString('base64'), 64);
+      }
+      resolve(certificate);
+    }
+  });
+}
+
+function get(url, timeout, port, protocol) {
+  validateUrl(url);
+
+  port = port || 443;
+  protocol = protocol || 'https:';
+
+  var options = getOptions(url, port, protocol);
+
+  return new Promise(function(resolve, reject) {
+    var req = handleRequest(options, resolve, reject);
+
+    if (timeout) {
+      req.setTimeout(timeout, function() {
+        reject({ message: 'rto' });
+        req.abort();
+      });
+    }
+
+    req.on('error', function(e) {
+      reject(e);
+    });
+
+    req.end();
+  });
+}
+
+module.exports = {
+  get: get
+};
+
---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: Artha Prihardana 
+ * @Date: 2019-08-26 17:07:40 
+ * @Last Modified by:   Artha Prihardana 
+ * @Last Modified time: 2019-08-26 17:07:40 
+ */
+var inquirer = require('inquirer')
+var check = require('./check.js')
+
+const questions = [
+	{
+		type: 'input',
+		name: 'address',
+		message: "Please input url address to check ssl validate? (https://example.com)",
+		validate: value => {
+			var reg = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/ig
+			var pass = value.match(reg);
+			return pass ? true : "Please enter a valid url address"
+		}
+	}
+]
+
+inquirer
+	.prompt(questions)
+	.then(answers => {
+		let addr = answers.address.split(/^((http[s]?|ftp):\/)?\/?/g);
+		check.get(addr[addr.length-1]).then(v => {
+			console.log(v)
+		}).catch(e => {
+			console.error(e)
+		})
+	});
